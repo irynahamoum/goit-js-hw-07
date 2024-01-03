af6b17e22be94e5ec63c5d04da149fd2498c493c
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-01-03 18:23:57 +0100

    corrected task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,20 @@
-const inputElement = document.getElementById("name-input");
-const spanElement = document.getElementById("name-output");
-
 // ======== 1 ========
+
+const textInput = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+textInput.addEventListener("input", (event) => {
+  const value = event?.currentTarget.value;
+  output.textContent = value.trim() || "Anonymous";
+});
 
 // ======== 2 ========
 
-inputElement.oninput = function (event) {
-  console.log(inputElement);
-  const value = event?.currentTarget.value;
-  spanElement.innerHTML = value.trim() || "Anonymous";
-};
+// const inputElement = document.getElementById("name-input");
+// const spanElement = document.getElementById("name-output");
+
+// inputElement.oninput = function (event) {
+//   console.log(inputElement);
+//   const value = event?.currentTarget.value;
+//   spanElement.innerHTML = value.trim() || "Anonymous";
+// };
